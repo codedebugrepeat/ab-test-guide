09d37d3fc4098cc658285a64366c17b416a55eca
codedebugrepeat <27236143+codedebugrepeat@users.noreply.github.com>
2026-04-22 10:13:35 +0200

    Suppress hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col font-sans">
         <SiteHeader />
